b8f19c96ab2f32f0377864c30d230f511ce07cd6
Timothy <timothy.curchod@accenture.com>
2022-11-06 02:59:36 +0900

    first passing test

--- src/useAnagrams.ts
+++ src/useAnagrams.ts
@@ -4,7 +4,21 @@ export function useAnagrams(input: any) {
   const [anagram, setState] = useState(input);
 
   const updateAnagream = (input: string) => {
-    const newVal = input + ' hi';
+    const newVal = input .split("")
+    .map((character: string, index: number) => {
+      const head = input.slice(0, index);
+      const tail = input.substring(index + 1);
+      const result: string[] = [];
+      for (let nextCharInTail of head) {
+        result.push(character + nextCharInTail);
+      }
+      for (let nextCharInTail of tail) {
+        result.push(character + nextCharInTail);
+      }
+      return result;
+    })
+    .join(" ");
+
     setState(newVal);
   }
 
