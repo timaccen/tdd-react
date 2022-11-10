c1e14c92c71bec100810240aebfb59c452d75155
Michael Park <mparkcode@gmail.com>
2022-11-10 01:30:29 +0000

    added possible combos function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,51 +9,51 @@ import {
 import App from "./App";
 import { useAnagrams } from "./useAnagrams";
 
-// test("should display anagrams of given input", () => {
-//   render(<App />);
-//   const nameInput = screen.getByLabelText(/Enter text/i);
-//   fireEvent.change(nameInput, { target: { value: "ab" } });
-//   fireEvent.click(screen.getByText(/submit/i));
-//   const expectedAnagrams = ["ab", "ba"];
-//   expectedAnagrams.forEach((anagram) => {
-//     expect(document.body.innerHTML).toMatch(anagram);
-//   });
-// });
+test("should display anagrams of given input", () => {
+  render(<App />);
+  const nameInput = screen.getByLabelText(/Enter text/i);
+  fireEvent.change(nameInput, { target: { value: "ab" } });
+  fireEvent.click(screen.getByText(/submit/i));
+  const expectedAnagrams = ["ab", "ba"];
+  expectedAnagrams.forEach((anagram) => {
+    expect(document.body.innerHTML).toMatch(anagram);
+  });
+});
 
-// test("should display anagrams of given input for biro", () => {
-//   const nameInput = screen.getByLabelText(/Enter text/i);
-//   fireEvent.change(nameInput, { target: { value: "biro" } });
-//   fireEvent.click(screen.getByText(/submit/i));
-//   const expectedAnagrams = [
-//     "biro",
-//     "bior",
-//     "brio",
-//     "broi",
-//     "boir",
-//     "bori",
-//     "ibro",
-//     "ibor",
-//     "irbo",
-//     "irob",
-//     "iobr",
-//     "iorb",
-//     "rbio",
-//     "rboi",
-//     "ribo",
-//     "riob",
-//     "roib",
-//     "robi",
-//     "obir",
-//     "obri",
-//     "oibr",
-//     "oirb",
-//     "orbi",
-//     "orib",
-//   ];
-//   expectedAnagrams.forEach((anagram) => {
-//     expect(document.body.innerHTML).toMatch(anagram);
-//   });
-// });
+test("should display anagrams of given input for biro", () => {
+  const nameInput = screen.getByLabelText(/Enter text/i);
+  fireEvent.change(nameInput, { target: { value: "biro" } });
+  fireEvent.click(screen.getByText(/submit/i));
+  const expectedAnagrams = [
+    "biro",
+    "bior",
+    "brio",
+    "broi",
+    "boir",
+    "bori",
+    "ibro",
+    "ibor",
+    "irbo",
+    "irob",
+    "iobr",
+    "iorb",
+    "rbio",
+    "rboi",
+    "ribo",
+    "riob",
+    "roib",
+    "robi",
+    "obir",
+    "obri",
+    "oibr",
+    "oirb",
+    "orbi",
+    "orib",
+  ];
+  expectedAnagrams.forEach((anagram) => {
+    expect(document.body.innerHTML).toMatch(anagram);
+  });
+});
 
 describe("useAnagrams hook", () => {
   it("checking default state", () => {
--- src/useAnagrams.ts
+++ src/useAnagrams.ts
@@ -3,18 +3,44 @@ import { useState } from "react";
 export function useAnagrams(input: any) {
   const [anagram, setState] = useState(input);
 
+  const getPossibleCombos = (input: string[]): any[] => {
+    const result = [] as any[];
+    input.map((character: string, index: number) => {
+      const firstCharacter = character;
+      if (index+1 < input.length){
+        const secondCharacter = input.slice(index+1, index+2);
+        console.log(`firstcharacter: ${firstCharacter}`)
+        console.log(`secondcharacter: ${secondCharacter}`)
+        const swap = [secondCharacter, firstCharacter].splice(index+2, input.length)
+        result.push(firstCharacter+secondCharacter)
+        result.push(secondCharacter+firstCharacter)
+      }
+    })
+    return result
+  }
+
   const updateAnagram = (input: string) => {
-    const newVal = input .split("")
+    const newVal = input.split("")
     .map((character: string, index: number) => {
-      const head = input.slice(0, index);
-      const tail = input.substring(index + 1);
+      console.log(`index: ${index}`)
+      const head = input.slice(0, index); 
+      console.log(`head: ${head}`)
+      const tail = input.substring(index, input.length).split('');
+      console.log(`Tail ${tail}`)
       const result: string[] = [];
-      for (let nextCharInTail of head) {
-        result.push(character + nextCharInTail);
+      if(head.length > 0){
+        for (let nextCharInTail of head) {
+          const possibleCombo = getPossibleCombos(tail)
+          console.log(`possibleCombo: ${possibleCombo}`)
+          for(let combo of possibleCombo){
+            result.push(head[0] + combo)
+          }
+        }
       }
-      for (let nextCharInTail of tail) {
-        result.push(character + nextCharInTail);
-      }
+      // for (let nextCharInTail of tail) {
+      //   result.push(character + nextCharInTail);
+      // }
+      console.log(`result: ${result}`)
       return result;
     })
     .join(" ");
