52938a85c981ce59d79289983646eeb84d5be9e7
Michael Park <mparkcode@gmail.com>
2022-11-24 00:06:36 +0000

    final solution with passing tests

--- src/useAnagrams.ts
+++ src/useAnagrams.ts
@@ -1,26 +1,50 @@
 import { useState } from "react";
 
+let wordStore: string[] = [];
+
 export function useAnagrams(input: any) {
   const [anagram, setState] = useState(input);
-
-  const updateAnagram = (input: string) => {
-    const newVal = input .split("")
-    .map((character: string, index: number) => {
-      const head = input.slice(0, index);
-      const tail = input.substring(index + 1);
-      const result: string[] = [];
-      for (let nextCharInTail of head) {
-        result.push(character + nextCharInTail);
-      }
-      for (let nextCharInTail of tail) {
-        result.push(character + nextCharInTail);
-      }
-      return result;
-    })
-    .join(" ");
-
-    setState(newVal);
+  const updateAnagram = (word: string) => {
+    const letters = word.split("");
+  letters.forEach((letter) => {
+    const dwindlingSupplyOfLetters = removeOneLetter(letters, letter);
+    chooseAndPass(letter, dwindlingSupplyOfLetters);
+  });
+    setState(wordStore.join(" "));
   }
-
-  return [anagram, updateAnagram ];
+  return [anagram, updateAnagram];
 }
+
+function chooseAndPass(wordString: string, remainingLetters: any) {
+  if (remainingLetters.length === 0) {
+    addWordToWordstore(wordString);
+  } else {
+    remainingLetters.forEach((newLetter: any) => {
+      const newWordString = wordString + newLetter;
+      const dwindlingSupplyOfLetters = removeOneLetter(
+        remainingLetters,
+        newLetter
+      );
+      chooseAndPass(newWordString, dwindlingSupplyOfLetters);
+    });
+  }
+}
+
+function removeOneLetter(remainingLetters: any, letter: any) {
+  let removed = false;
+  return remainingLetters.filter((currentItem: any) => {
+    if (currentItem === letter && !removed) {
+      removed = true;
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
+
+function addWordToWordstore(newWord: any) {
+  if (!wordStore.includes(newWord)) {
+    wordStore.push(newWord);
+  }
+  wordStore.sort();
+} 
